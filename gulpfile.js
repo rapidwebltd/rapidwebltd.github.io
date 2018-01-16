9759b980760c2ba7532883c5fa0d08b609c86e49
Ollie Reardon <oliver.reardon@rapidweb.biz>
2018-01-16 16:36:08 +0000

    Local dependencies added

--- gulpfile.js
+++ gulpfile.js
@@ -1,100 +1,63 @@
-/*Project Vars*/
-var html_dir = ['./*.html', './*.php', './**/*.html', './**/*.php'],
-    less_dir = ['./less/style.less'],
-    js_dir = ['./bower_components/jquery/dist/jquery.min.js', 
-    './bower_components/bootstrap/js/ie10-viewport-bug-workaround.js', 
-    './bower_components/bootstrap/dist/js/bootstrap.min.js',
+var sass_dir = [
+    './scss/style.scss'],
+    js_dir = [
+    './node_modules/jquery/dist/jquery.js',
+    './node_modules/popper.js/dist/umd/popper.js',
+    './node_modules/bootstrap/dist/js/bootstrap.js',
+    './node_modules/wowjs/dist/wow.min.js',
     './js/scripts.js'],
     js_output_dir = './public/js',
     css_dir = './public/css',
-    php_dir = ['./*.php', './**/*.php'],
-    img_src_dir = ['./*.+(png|jpg|gif|jpeg)', './**/*.+(png|jpg|gif|jpeg)'],
-    img_out_dir = './public/img',
-    fonts_dir = ['./public/bower_components/font-awesome/fonts/*'],
-    fonts_output_dir = './public/fonts';
+    fonts_dir = ['./node_modules/font-awesome/fonts/*'],
+    fonts_output_dir = './public/fonts/';
 
-/*Gulp Requires*/
 var gulp = require('gulp'),
-    less = require('gulp-less'),
-    LessPluginCleanCSS = require('less-plugin-clean-css'),
     watch = require('gulp-watch'),
     path = require('path'),
-    bootlint  = require('gulp-bootlint'),
     uglify = require('gulp-uglify'),
-    jshint = require('gulp-jshint'),
     cat = require('gulp-concat'),
-    cleancss = new LessPluginCleanCSS({ advanced: true, keepSpecialComments: 0 }),
-    imageop = require('gulp-image-optimization'),
-    livereload = require('gulp-livereload');
-    
-/*Default Task*/
-gulp.task('default', ['less', 'js-build', 'images']);
+    rename = require('gulp-rename'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer');
 
-/*Run linters*/
-gulp.task('lint', ['bootlint', 'jshint']);
+gulp.task('default', ['sass', 'js-build', 'fonts']);
 
 /*Watch Task*/
 gulp.task('watch', function() {
-    livereload.listen();
-    gulp.watch(html_dir, ['bootlint']);
-    gulp.watch(less_dir, ['less']);
-    gulp.watch(js_dir, ['js-build-dev', 'jshint']);
+    gulp.watch(sass_dir, ['sass-main']);
+    gulp.watch(js_dir, ['js-build-dev']);
 });
-
-/* Image Min */
-gulp.task('images', function(cb) {
-    gulp.src(img_src_dir).pipe(imageop({
-        optimizationLevel: 8,
-        progressive: true,
-        interlaced: true
-    })).pipe(gulp.dest(img_out_dir)).on('end', cb).on('error', cb);
-});
-
 /*Compile LESS*/
-gulp.task('less', ['less-main']);
+gulp.task('sass', ['sass-main']);
+gulp.task('scss', ['sass-main']);
+gulp.task('less', ['sass-main']);
 
-gulp.task('less-main', function() {
-    return gulp.src(less_dir)
-        .pipe(less({
-          paths: [ path.join(__dirname, 'less', 'includes') ],
-          plugins: [cleancss]
-        })).pipe(livereload())
-        .pipe(gulp.dest(css_dir));
+gulp.task('sass-main', function() {
+    return gulp
+    .src(sass_dir)
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(autoprefixer())
+    .pipe(cat('style.min.css'))
+    .pipe(gulp.dest(css_dir));
 });
 
 /*Compile JS*/
-gulp.task('js-build', ['js-build-main', 'jshint']);
+gulp.task('js-build', ['js-build-main']);
 
 gulp.task('js-build-main', function() {
   return gulp.src(js_dir)
-    .pipe(uglify())
     .pipe(cat('scripts.min.js'))
+    .pipe(uglify())
     .pipe(gulp.dest(js_output_dir))
-    .pipe(livereload());
 });
 
 /*Compile JS UNMINIFIED!!*/
-gulp.task('js-build-dev', ['js-build-main-dev', 'jshint']);
+gulp.task('js-build-dev', ['js-build-main-dev']);
 
 gulp.task('js-build-main-dev', function() {
   return gulp.src(js_dir)
     .pipe(cat('scripts.min.js'))
     .pipe(gulp.dest(js_output_dir))
-    .pipe(livereload());
-});
-
-/*Bootlint*/
-gulp.task('bootlint', function() {
-    return gulp.src(html_dir)
-        .pipe(bootlint({disabledIds: ['E001', 'W001', 'W002', 'W003', 'W005']}))
-        .pipe(livereload());
-    
-});
-/*JS Lint*/
-gulp.task('jshint', function() {
-  return gulp.src(js_dir)
-    .pipe(jshint())
-    .pipe(jshint.reporter('jshint-stylish'));
 });
 
 /*Copy FontAwesome fonts to the public directory*/
--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,48 @@
-/*  this is where project js will go */
+//Web font load
+      WebFontConfig = {
+        google: { families: [ 'Open+Sans::latin' ] }
+      };
+      (function() {
+        var wf = document.createElement('script');
+        wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+        '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+        wf.type = 'text/javascript';
+        wf.async = 'true';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(wf, s);
+      })(); 
+//Scroll to projects
+$(document).ready(function (){
+	new WOW().init();
+	$(".btn-projects").click(function (){
+		$('html, body').animate({
+			scrollTop: $(".project-title").offset().top
+		}, 1000);
+	});
+
+	var repos, packages = [];
+	$.getJSON('https://packagist.org/packages/list.json?vendor=rapidwebltd', function(repos) {
+
+		for (i = 0; i < repos.packageNames.length; i++) {
+			$.getJSON('https://packagist.org/packages/'+repos.packageNames[i]+'.json', function(package) {
+				var package  = package.package;
+				var title = package.name;
+				title = title.replace('rapidwebltd/', '');
+				title = title.replace(/-/g, ' ');
+				packages.push(package);
+				console.log(package);
+
+				$('#packages').append('<div class="card text-center wow fadeIn" data-wow-delay="0.5s" data-wow-offset="5"><div class="card-body"><h5 class="card-title text-capitalize">'+title+'</h5><p class="card-text">Downloads: <span class="badge badge-info">'+package.downloads.total+'</span></p><p class="card-text text-left">'+package.description+'</p><p class="text-center m-0"><a href="'+package.repository+'" class="btn btn-outline-info btn-lg btn-block">View Project</a></p></div></div>');
+
+			});
+
+		}
+
+		packages.sort(function(a, b){
+			console.log(a);
+			a.downloads.total-b.downloads.total
+		});
+         //console.log(packages); 
+
+     });
+});
